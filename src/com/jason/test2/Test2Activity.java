84fa19dc2041e1bd21584d62647860053bda39d9
jaesungPark <hotpjs1@gmail.com>
2012-06-02 16:48:59 +0900

    update2

--- src/com/jason/test2/Test2Activity.java
+++ src/com/jason/test2/Test2Activity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class Test2Activity extends Activity {
     /** Called when the activity is first created. */
-	////
+	//////
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
